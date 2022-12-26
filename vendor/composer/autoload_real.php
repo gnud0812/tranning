b9c20b4b071f04cd020f1fefe9b0959d53bc8a94
Tri Nguyen <nguyeminhtrii@gmail.com>
2022-12-26 13:54:27 +0700

    setup file

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,18 +33,25 @@ class ComposerAutoloaderInit193912dddada3cf79a3020122e423a16
 
         $loader->register(true);
 
-        $filesToLoad = \Composer\Autoload\ComposerStaticInit193912dddada3cf79a3020122e423a16::$files;
-        $requireFile = static function ($fileIdentifier, $file) {
-            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
-                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
-
-                require $file;
-            }
-        };
-        foreach ($filesToLoad as $fileIdentifier => $file) {
-            ($requireFile)($fileIdentifier, $file);
+        $includeFiles = \Composer\Autoload\ComposerStaticInit193912dddada3cf79a3020122e423a16::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire193912dddada3cf79a3020122e423a16($fileIdentifier, $file);
         }
 
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire193912dddada3cf79a3020122e423a16($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
