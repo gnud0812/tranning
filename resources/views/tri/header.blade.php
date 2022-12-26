4e56daa337c9665c189d45f49297e4fd10c30a5e
Tri Nguyen <nguyeminhtrii@gmail.com>
2022-12-26 20:12:05 +0700

    final topbar

--- resources/views/tri/header.blade.php
+++ resources/views/tri/header.blade.php
@@ -1,46 +1,45 @@
 <link rel="stylesheet" href="{{asset('css/Tri/tri.css')}}">
 
-<h1>abc</h1>
-<div>
-<nav class="navbar header-navbar pcoded-header">
-            <div class="navbar-wrapper">
-                <div class="navbar-logo">
-                    <a class="mobile-menu" id="mobile-collapse" href="#!">
-                        <i class="feather icon-menu"></i>
-                    </a>
-                    <a href="/images/Tri/logo.png">
-                        <img class="img-fluid" src="images\Tri\images\logo.png" alt="Theme-Logo">
-                    </a>
-                    <a class="mobile-options">
-                        <i class="feather icon-more-horizontal"></i>
-                    </a>
-                </div>
-                <div class="navbar-container container-fluid">
-                    <ul class="nav-left">
-                        <li class="header-search">
-                            <div class="main-search morphsearch-search">
-                                <div class="input-group">
-                                    <span class="input-group-addon search-close"><i class="feather icon-x"></i></span>
-                                    <input type="text" class="form-control">
-                                    <span class="input-group-addon search-btn"><i class="feather icon-search"></i></span>
-                                </div>
-                            </div>
-                        </li>
-                        <li>
-                            <a href="#!" onclick="javascript:toggleFullScreen()">
-                                <i class="feather icon-maximize full-screen"></i>
-                            </a>
-                        </li>
-                    </ul>
-                    <ul class="nav-right">
-                        <li class="header-notification">
-                        </li>
-                        <li class="header-notification">
-                        </li>
-                        <li class="user-profile header-notification">
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+<div id="pcoded" class="pcoded">
+	<div class="pcoded-container navbar-wrapper">
+		<div class="nav-left">
+			<div class="header-search">
+				<div class="main-search morphsearch-search">
+					<div class="input-group">
+						<span class="input-group-addon search-btn"><i class="feather icon-search"></i></span>
+					</div>
+				</div>
+			</div>
+			<div>
+				<a href="#!" class="full-screenn">
+					<i class="feather icon-maximize full-screen"></i>
+				</a>
+			</div>
+		</div>
+		<div class="nav-right">
+			<div class="header-notification">
+				<div class="dropdown-primary dropdown">
+					<div class="dropdown-toggle" data-toggle="dropdown">
+						<i class="feather icon-bell"></i>
+					</div>
+				</div>
+			</div>
+			<div class="header-notification">
+				<div class="dropdown-primary dropdown">
+					<div class="displayChatbox dropdown-toggle" data-toggle="dropdown">
+						<i class="feather icon-message-square"></i>
+					</div>
+				</div>
+			</div>
+			<div class="user-profile header-notification">
+				<div class="dropdown-primary dropdown">
+					<div class="dropdown-toggle" data-toggle="dropdown">
+						<img src="images\Tri\images\avatar-4.jpg" class="img-radius" alt="User-Profile-Image">
+						<span class="text">John Doe</span>
+						<i class="feather icon-chevron-down"></i>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
