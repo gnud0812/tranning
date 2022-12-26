ddc8f2692a7f06e022a6aab120b8c11e37794f1d
MorelloComicon <91968892+MorelloComicon@users.noreply.github.com>
2022-12-26 15:02:09 +0700

    update sidebar

---
+++ resources/views/tan/tpl_sidebar.blade.php
@@ -0,0 +1,106 @@
+<link rel="stylesheet" href="{{ asset('css/Tan/tan.css') }}">
+
+<div class="sidebar-wrapper">
+    <div class="sidebar">
+        <ul>
+            <li class="">
+                <div class="">Navigation</div>
+                <ul>
+                    <li>Dashboard</li>
+                    <li>Page layouts</li>
+                    <li>Navigation</li>
+                    <li>Widget</li>
+                </ul>
+            </li>
+            <li class="">
+                <div class="">UI Elements</div>
+                <ul>
+                    <li>Basic Components</li>
+                    <li>Advanced Components</li>
+                    <li>Extra Components</li>
+                    <li>Animations</li>
+                    <li>Sticky Notes</li>
+                    <li>Icons</li>
+                </ul>
+            </li>
+            <li class="">
+                <div class="">Forms</div>
+                <ul>
+                    <li>Form Components</li>
+                    <li>Form Picker</li>
+                    <li>Form Select</li>
+                    <li>Form Masking</li>
+                    <li>Form Wizard</li>
+                    <li>Ready To Use</li>
+                </ul>
+            </li>
+            <li class="">
+                <div class="">Table</div>
+                <ul>
+                    <li>Bootstrap Table</li>
+                    <li>Data Table</li>
+                    <li>Data Table Extensions</li>
+                    <li>FooTable</li>
+                    <li>Handson Table</li>
+                    <li>Editable Table</li>
+                </ul>
+            </li>
+            <li class="">
+                <div class="">Chart And Maps</div>
+                <ul>
+                    <li>Charts</li>
+                    <li>Maps</li>
+                    <li>Landing Page</li>
+                </ul>
+            </li>
+            <li class="">
+                <div class="">Pages</div>
+                <ul>
+                    <li>Authentication</li>
+                    <li>Maintenance</li>
+                    <li>User Profile</li>
+                    <li>E-Commerce</li>
+                    <li>Email</li>
+                </ul>
+            </li>
+            <li class="">
+                <div class="">App</div>
+                <ul>
+                    <li>Chat</li>
+                    <li>Social</li>
+                    <li>Task</li>
+                    <li>To-Do</li>
+                    <li>Gallery</li>
+                    <li>Search</li>
+                    <li>Job Search</li>
+                </ul>
+            </li>
+            <li class="">
+                <div class="">Extension</div>
+                <ul>
+                    <li>Editor</li>
+                    <li>Invoice</li>
+                    <li>Event Calendar</li>
+                    <li>Image Cropper</li>
+                    <li>File Upload</li>
+                    <li>Change Loges</li>
+                </ul>
+            </li>
+            <li class="">
+                <div class="">Other</div>
+                <ul>
+                    <li>Menu Levels</li>
+                    <li>Disable Menu</li>
+                    <li>Sample Page</li>
+                </ul>
+            </li>
+            <li class="">
+                <div class="">Support</div>
+                <ul>
+                    <li>Documentation</li>
+                    <li>Submit Issue</li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
